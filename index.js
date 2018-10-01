5a5e21f2bf731e7e333b38cbca1423d7d30f41eb
Mark Heramis <chumheramis@gmail.com>
2018-10-01 19:41:33 +0800

    Initial commit
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

---
+++ index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-360';
+
+export default class react360 extends React.Component {
+  render() {
+    return (
+      <View style={styles.panel}>
+        <View style={styles.greetingBox}>
+          <Text style={styles.greeting}>
+            Welcome to the SSV Normandy SR1 Bridge
+          </Text>
+        </View>
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  panel: {
+    // Fill the entire surface
+    width: 1000,
+    height: 600,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  greetingBox: {
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    borderColor: '#639dda',
+    borderWidth: 2,
+  },
+  greeting: {
+    fontSize: 30,
+  },
+});
+
+AppRegistry.registerComponent('react360', () => react360);
